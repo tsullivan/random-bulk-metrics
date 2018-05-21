3a88e69be1e42c1fbeb90e3e2f5e011f76114632
Timothy Sullivan <tsullivan@elastic.co>
2018-05-20 21:47:55 -0700

    make it a sales index

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 #! /usr/bin/env node
+const INDEX_NAME = 'sales';
+
+const { getSettings, getData } = require('./lib');
 const argv = require('yargs').argv;
-const { getData } = require('./lib');
 const data = getData(argv);
-for (const d of data) {
-  console.log(d); // eslint-disable-line no-console
+
+/* eslint-disable no-console */
+console.log(`DELETE /${INDEX_NAME}`);
+console.log('\n');
+
+console.log(getSettings(INDEX_NAME));
+console.log('\n');
+
+console.log(`POST /${INDEX_NAME}/_bulk`);
+for (const doc of data) {
+  console.log(doc);
 }

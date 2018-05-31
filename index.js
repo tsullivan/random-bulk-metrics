87be178b8ee7b5cdd0c97d00837fd62e2e0956f5
Timothy Sullivan <tsullivan@elastic.co>
2018-05-31 12:51:18 -0700

    logIt

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 #! /usr/bin/env node
+
+/* eslint-disable no-console */
 const INDEX_NAME = 'sales';
 
 const { getSettings, getData } = require('./lib');
 const argv = require('yargs').argv;
 const data = getData(argv);
+const logIt = () => {
+  console.log(`DELETE /${INDEX_NAME}`);
+  console.log('\n');
 
-/* eslint-disable no-console */
-console.log(`DELETE /${INDEX_NAME}`);
-console.log('\n');
+  console.log(getSettings(INDEX_NAME));
+  console.log('\n');
 
-console.log(getSettings(INDEX_NAME));
-console.log('\n');
+  console.log(`POST /${INDEX_NAME}/_bulk`);
+  for (const doc of data) {
+    console.log(doc);
+  }
+};
 
-console.log(`POST /${INDEX_NAME}/_bulk`);
-for (const doc of data) {
-  console.log(doc);
-}
+logIt();

e916b3854c32e37b8cf7e1190102dc8b7786b4c4
Timothy Sullivan <tsullivan@elastic.co>
2020-06-24 16:05:06 -0700

    pattern of indices

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
 #! /usr/bin/env node
 
-// eslint-disable-next-line no-unused-vars
 // eslint-disable-next-line no-console
 const lag = (message) => console.log(message);
-const INDEX_NAME = 'tests';
+const INDEX_PREFIX = 'tests-';
 
 const { getSettings, getData } = require('./lib');
 const argv = require('yargs').argv;
-const data = getData(argv);
+
+const data1 = getData(argv, 1);
+const data2 = getData(argv, 2);
+const data3 = getData(argv, 3);
+
 const logIt = () => {
-  lag(`DELETE /${INDEX_NAME}`);
+  lag(`DELETE /${INDEX_PREFIX}*`);
   lag('\n');
 
-  lag(getSettings(INDEX_NAME));
+  lag(getSettings(INDEX_PREFIX));
   lag('\n');
 
-  lag(`POST /${INDEX_NAME}/_bulk`);
-  for (const doc of data) {
+  lag(`POST /${INDEX_PREFIX}001/_bulk`);
+  for (const doc of data1) {
+    lag(doc);
+  }
+
+  lag(`POST /${INDEX_PREFIX}002/_bulk`);
+  for (const doc of data2) {
+    lag(doc);
+  }
+
+  lag(`POST /${INDEX_PREFIX}003/_bulk`);
+  for (const doc of data3) {
     lag(doc);
   }
 };

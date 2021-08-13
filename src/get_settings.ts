8c10059bca9981738a3c059c4223708e03811b68
Timothy Sullivan <tsullivan@elastic.co>
2021-08-12 17:42:11 -0700

    chanes

--- src/get_settings.ts
+++ src/get_settings.ts
@@ -1,10 +1,8 @@
 import { MappingTypeValue } from './lib/field_definition';
 import { fields } from './fields';
 
-type MappingProperties = Record<string, { type: MappingTypeValue }>;
-
-export function getSettings(INDEX_PREFIX: string) {
-  const mappingProperties: MappingProperties = fields.reduce(
+export function getSettings() {
+  const properties: Record<string, { type: MappingTypeValue }> = fields.reduce(
     (acc, field) => ({
       ...acc,
       [field.name]: { type: field.type },
@@ -12,25 +10,18 @@ export function getSettings(INDEX_PREFIX: string) {
     {}
   );
 
-  const settings = {
+  const runtime = {
+    ab_sum: {
+      type: 'long',
+      script: { source: `emit(doc['field_a'].value + doc['field_b'].value)` },
+    },
+  } ;
+
+  return {
     settings: { number_of_shards: 1, number_of_replicas: 0 },
     mappings: {
-      properties: mappingProperties,
-      runtime: {
-        ab_sum: {
-          type: 'long',
-          script: { source: `emit(doc['field_a'].value + doc['field_b'].value)` },
-        },
-      },
+      properties,
+      runtime,
     },
   };
-
-  const output = [];
-  output.push(`PUT /_index_template/${INDEX_PREFIX}dev`);
-  output.push('{');
-  output.push(`  "index_patterns": ["${INDEX_PREFIX}*"],`);
-  output.push('  "priority": 1,');
-  output.push(`  "template":\n    ${JSON.stringify(settings)}`);
-  output.push('}');
-  return output.join('\n');
 }
--- src/index.ts
+++ src/index.ts
@@ -7,30 +7,15 @@ const lag = (message: string) => console.log(message);
 const INDEX_PREFIX = 'tests-';
 
 const data1 = getData(argv, 1);
-const data2 = getData(argv, 2);
-const data3 = getData(argv, 3);
 
 const logIt = () => {
   lag(`DELETE /${INDEX_PREFIX}001`);
-  lag(`DELETE /${INDEX_PREFIX}002`);
-  lag(`DELETE /${INDEX_PREFIX}003`);
-  lag('\n');
 
-  lag(getSettings(INDEX_PREFIX));
-  lag('\n');
+  lag(`PUT /_template/${INDEX_PREFIX}dev`);
+  lag(`{ "index_patterns": ["${INDEX_PREFIX}*"], "template": ${JSON.stringify(getSettings())} }`);
 
-  lag(`POST /${INDEX_PREFIX}001/_bulk`);
+  lag(`POST /${INDEX_PREFIX}001/_doc/_bulk`);
   for (const doc of data1) {
-    lag(doc);
-  }
-
-  lag(`POST /${INDEX_PREFIX}002/_bulk`);
-  for (const doc of data2) {
-    lag(doc);
-  }
-
-  lag(`POST /${INDEX_PREFIX}003/_bulk`);
-  for (const doc of data3) {
     lag(doc);
   }
 };

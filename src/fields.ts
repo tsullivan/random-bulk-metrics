49e2ca6cee8077e96069ad81ba99fa8af1573c93
Timothy Sullivan <tsullivan@elastic.co>
2022-06-29 16:36:36 -0700

    add nested field

--- src/fields.ts
+++ src/fields.ts
@@ -5,13 +5,15 @@ import { sample } from 'lodash';
 type CharGroup = 'vowels' | 'consos' | 'other';
 type CharGroups = Record<CharGroup, string[]>;
 
+const { ceil, random, round } = Math;
+
 const charGroups: CharGroups = {
   vowels: ['a', 'e', 'i', 'o', 'u'],
   consos: ['f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'y'],
   other: ['慮', '慮', '慶', '畬', '獥', '漠', '敭', 'Ⱒ'],
 };
 
-export const fields: FieldDefinition<number | string | null>[] = [
+export const fields: FieldDefinition[] = [
   <FieldDefinition<string>>{
     name: 'timestamp',
     type: 'date',
@@ -27,7 +29,7 @@ export const fields: FieldDefinition<number | string | null>[] = [
       if (iteration % 8 === 0) {
         return sample(charGroups['other']) as string;
       }
-      const term = Array(3 + Math.ceil(Math.random() * 7));
+      const term = Array(3 + ceil(random() * 7));
       for (let i = term.length; i > 0; i--) {
         const kind = sample(['vowels', 'vowels', 'consos']) as CharGroup;
         const particle = sample(charGroups[kind]) as string;
@@ -40,10 +42,41 @@ export const fields: FieldDefinition<number | string | null>[] = [
     name: 'updated_at',
     type: 'date',
     getValue(time, iteration) {
-      if (iteration > 500 && iteration % 11 === 0) {
-        return moment.utc(time).subtract(2, 'days').format();
-      }
-      return null;
+      return moment.utc(time).subtract(2, 'days').format();
     },
   },
+  <FieldDefinition<any>>{
+    name: 'articles',
+    type: 'nested',
+    getValue() {
+      return new ArticleDocumentSet().getInnerDocuments();
+    },
+  }
 ];
+
+interface IArticleDocument {
+  language: string;
+  type: string;
+  titel: string;
+  id: number;
+}
+
+class ArticleDocumentSet {
+  private createTitel() {
+    return sample([
+      'Welcome to the Knowledge Base.',
+      'Orders',
+      'Bestellung öffnen',
+      'Confirm Order & Confirmation Status',
+      'Access to your order',
+    ]) as string;
+  }
+  public getInnerDocuments(): IArticleDocument {
+    return ({
+      language: 'en',
+      type: 'article',
+      titel: this.createTitel(),
+      id: round(random() * 1000 + 1000),
+    });
+  }
+}
--- src/lib/field_definition.ts
+++ src/lib/field_definition.ts
@@ -1,6 +1,6 @@
-export type MappingTypeValue = 'date' | 'keyword' | 'float' | 'integer' | 'ip' | 'object';
+export type MappingTypeValue = 'date' | 'keyword' | 'float' | 'integer' | 'ip' | 'object' | 'nested';
 
-export interface FieldDefinition<T extends number | string | null> {
+export interface FieldDefinition<T = number | string | null> {
   readonly name: string;
   readonly type: MappingTypeValue;
   getValue(time: number, iteration: number): T | Promise<T>;

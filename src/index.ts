dc0cea91174c947e47d3388e7886e20046129ce0
Timothy Sullivan <tsullivan@elastic.co>
2022-06-30 10:08:16 -0700

    articles

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const argv = require('yargs').argv;
 
 // eslint-disable-next-line no-console
 const lag = (message: string) => console.log(message);
-const INDEX_PREFIX = 'logstash-';
+const INDEX_PREFIX = 'articles-';
 
 const data = getData(argv, 1);
 

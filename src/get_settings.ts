8f74da353d4c2623648525372711b5c99d8e9372
Timothy Sullivan <tsullivan@elastic.co>
2022-07-05 12:26:23 -0700

    simple

--- src/get_settings.ts
+++ src/get_settings.ts
@@ -33,7 +33,7 @@ export function getSettings() {
   }, {});
 
   return {
-    settings: { number_of_shards: 1, number_of_replicas: 0 },
+    settings: { number_of_shards: 1, number_of_replicas: 1 },
     mappings: { properties },
   };
 }
